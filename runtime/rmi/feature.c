368b1f7b460e9c45bf112f74932e52b3150e5d8b
Jean-Philippe Brucker <jean-philippe@linaro.org>
2024-05-31 10:12:17 +1000

    TMP: fix(qemu): Support lower PMU versions
    Although RME mandates v3p7, QEMU has v3p5 at the moment. Until we
    implement the p7 extension, which provides counter isolation for EL3,
    allow p5.
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>
    Change-Id: Id1dd30486f203187e3f882c7495116b743d3883a

--- runtime/rmi/feature.c
+++ runtime/rmi/feature.c
@@ -33,7 +33,7 @@ unsigned long get_feature_register_0(void)
 						RMI_FEATURE_TRUE);
 
 	/* RMM supports PMUv3p7+ */
-	assert(read_pmu_version() >= ID_AA64DFR0_EL1_PMUv3p7);
+	assert(read_pmu_version() >= ID_AA64DFR0_EL1_PMUv3p5);
 
 	/* Set support for PMUv3 */
 	feat_reg0 |= INPLACE(RMM_FEATURE_REGISTER_0_PMU_EN,
