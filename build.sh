b0d5f4098e40158f1899c48d2a2ae832d2618d95
Gavin Shan <gshan@redhat.com>
2024-10-16 17:26:19 +1000

    Add build.sh
    Add build.sh to build the target automatically.
    
    Signed-off-by: Gavin Shan <gshan@redhat.com>

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+export | grep CROSS_COMPILE > /dev/null
+if [ $? -ne 0 ]; then
+   export CROSS_COMPILE=aarch64-none-elf-
+fi
+
+cmake -DCMAKE_BUILD_TYPE=Release -DRMM_CONFIG=fvp_defcfg -DLOG_LEVEL=40 -B build
+cmake --build build
