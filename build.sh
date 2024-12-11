4e2e7643d8d667a5ee2e8064e15cc1473f28f444
Gavin Shan <gshan@redhat.com>
2024-12-11 17:49:34 +1000

    Add build.sh
    Add build.sh to build the target automatically.
    
    Signed-off-by: Gavin Shan <gshan@redhat.com>

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]; then
+   echo "./build fvp|qemu"
+   exit 1
+fi
+
+export | grep CROSS_COMPILE > /dev/null
+if [ $? -ne 0 ]; then
+   export CROSS_COMPILE=aarch64-none-elf-
+fi
+
+if [ "$1" == "qemu" ]; then
+   cmake -DCMAKE_BUILD_TYPE=Debug       \
+         -DRMM_CONFIG=qemu_virt_defcfg  \
+         -DLOG_LEVEL=40                 \
+         -B build-qemu
+   cmake --build build-qemu
+elif [ "$1" == "fvp" ]; then
+   cmake -DCMAKE_BUILD_TYPE=Debug       \
+         -DRMM_CONFIG=fvp_defcfg        \
+         -DLOG_LEVEL=40                 \
+         -B build
+   cmake --build build
+else
+   echo "./build.sh fvp|qemu"
+   exit 1
+fi
