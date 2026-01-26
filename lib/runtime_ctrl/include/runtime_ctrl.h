3225a0a76a620b659b2af9a6740ce3a5725f5cd8
Javier Almansa Sobrino <javier.almansasobrino@arm.com>
2026-01-26 19:38:23 +0000

    feat(runtime_ctx): add runtime_ctx library to control sro flow
    This patch introduces the runtime_ctx library, used by SRO RMI
    operations to query if the command must exit (e.g. with RMI_CONTINUE)
    or can continue.
    
    Signed-off-by: Javier Almansa Sobrino <javier.almansasobrino@arm.com>
    Change-Id: Ia053f742330a274d3add26e5d805c16d9df568a8

---
+++ lib/runtime_ctrl/include/runtime_ctrl.h
@@ -0,0 +1,66 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * SPDX-FileCopyrightText: Copyright TF-RMM Contributors.
+ */
+
+#ifndef RUNTIME_CTL_H
+#define RUNTIME_CTL_H
+
+#include <stdbool.h>
+
+/*
+ * The `runtime control` library monitors specific conditions on RMI handlesr
+ * and decides if it is the time to return to the Host.
+ * Usage:
+ *  - rtc_start:       The RMM should call it at the start of the running.
+ *  - rtc_set_command: The RMM  should call it once it knows what handler
+ *		       is running. For rmi_op_*, this is after the SRO context
+ *		       is open.
+ *  - rtc_exit:        The handlers should call it periodically to check if it
+ *		       is time to return to the Host.
+ */
+
+/*
+ * Callback prototype to be invoked to check if a command can continue or not.
+ *
+ * 	Input:
+ * 		- args: Pointer to a generic data structure passed down from
+ * 			the caller to rtc_exit(). There is no sanitisation
+ * 			of the input so it is expected that the caller of
+ * 			rtc_exit() passes a pointer to a data structure which
+ * 			is compatible with the one expected by the callback.
+ * 			The pointer can be NULL if no arguments are needed.
+ *
+ * 	Return: 'True' if the command must exit (e.g. RMI_CONTINUE) or 'False'
+ * 		otherwise.
+ */
+typedef bool (*rtc_check_exit_cb)(void *args);
+
+/*
+ * Set the current command being processed by the current CPU.
+ * If a command will need to query the library, this API will need to be
+ * called upon RMI entry as well as on its continue handlers.
+ */
+void rtc_set_command(unsigned long fid);
+
+/*
+ * Query if a command needs to exit (e.g with RMI_CONTINUE) or can
+ * continue execution.
+ *
+ *	Input:
+ *		- args: Pointer to a generic data structure which
+ *			will be passed to the callbacks registered for
+ *			the current command in progress.
+ *			Note that there is no sanitisation of the input
+ *			so it is expected that the caller passes a pointer
+ *			to the same structure type which will be used as
+ *			parameter for the callbacks registered with
+ *			rtc_register_callback(). Note that the pointer is
+ *			allowed to be NULL if no args are required.
+ *
+ * 	Return: 'True' if the command must exit (e.g. RMI_CONTINUE) or 'False'
+ * 		if it can continue execution.
+ */
+bool rtc_exit(void *args);
+
+#endif /* RUNTIME_CTL_H */
---
+++ lib/runtime_ctrl/src/runtime_ctrl.c
@@ -0,0 +1,59 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * SPDX-FileCopyrightText: Copyright TF-RMM Contributors.
+ */
+
+#include <assert.h>
+#include <cpuid.h>
+#include <debug.h>
+#include <runtime_ctrl.h>
+#include <smc-rmi.h>
+#include <smc.h>
+
+/* List of current FID being executed by each CPU */
+static unsigned long current_fid[MAX_CPUS] = {0UL};
+
+struct rtc_handlers {
+	rtc_check_exit_cb cb;
+};
+
+#define RTC_HANDLER(_id, _cb)[RMI_HANDLER_ID(SMC_RMI_##_id)] = {	\
+	.cb = (_cb)							\
+}
+
+struct rtc_handlers rtc_handlers[] = {
+	{NULL}
+};
+COMPILER_ASSERT(ARRAY_SIZE(rtc_handlers) <= SMC64_NUM_FIDS_IN_RANGE(RMI));
+
+
+void rtc_set_command(unsigned long fid)
+{
+	assert(current_fid[my_cpuid()] == 0UL);
+	assert(fid < SMC64_NUM_FIDS_IN_RANGE(RMI));
+
+	current_fid[my_cpuid()] = fid;
+}
+
+bool rtc_exit(void *args)
+{
+	unsigned int handler_id = RMI_HANDLER_ID(current_fid[my_cpuid()]);
+	bool retval;
+
+	assert(handler_id < ARRAY_SIZE(rtc_handlers));
+	assert(rtc_handlers[handler_id].cb != NULL);
+
+	retval = rtc_handlers[handler_id].cb(args);
+
+	if (retval) {
+		/*
+		 * Reset the FID for the current CPU. It is the CONTINUE
+		 * handler for the current operation responsibility to call
+		 * rtc_set_command() again upon entry if later queries for
+		 * exit are needed.
+		 */
+	       current_fid[my_cpuid()] = 0UL;
+	}
+
+	return retval;
+}
