c2b0e68bdc7ee097c2f7134359f6785e0bf931d0
Shruti Gupta <shruti.gupta@arm.com>
2026-02-25 12:30:37 +0000

    feat(rmi): update EL3 interface version to 2.0
    Change-Id: Iddb29b366496fe7224cb8b9c5cce46bc04fc05c4
    Signed-off-by: Shruti Gupta <shruti.gupta@arm.com>

--- lib/rmm_el3_ifc/src/rmm_el3_runtime.c
+++ lib/rmm_el3_ifc/src/rmm_el3_runtime.c
@@ -264,7 +264,7 @@ int rmm_el3_ifc_get_feat_register(unsigned int feat_reg_idx, uint64_t *feat_reg)
 	struct smc_args smc_args = SMC_ARGS_1(feat_reg_idx);
 	unsigned long rmm_el3_ifc_version = rmm_el3_ifc_get_version();
 
-	if ((RMM_EL3_IFC_GET_VERS_MAJOR(rmm_el3_ifc_version) != 0U) ||
+	if ((RMM_EL3_IFC_GET_VERS_MAJOR(rmm_el3_ifc_version) == 0U) &&
 		(RMM_EL3_IFC_GET_VERS_MINOR(rmm_el3_ifc_version) < 4U)) {
 		ERROR("Feature register access not supported by this version 0x%lx\n",
 			rmm_el3_ifc_version);
